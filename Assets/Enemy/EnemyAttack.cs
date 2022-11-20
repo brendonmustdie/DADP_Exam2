8e01cb292af9ac68eabd4b21fc87f24efb375d45
brendonmustdie <91871627+brendonmustdie@users.noreply.github.com>
2022-11-20 21:02:29 +0200

    enemies

--- Assets/Enemy/EnemyAttack.cs
+++ Assets/Enemy/EnemyAttack.cs
@@ -7,4 +7,30 @@ public class EnemyAttack : MonoBehaviour
     //if player enters trigger,
     //enemy becomes awake, bring up exclamation mark
     //enemy chases after player
+
+    Vector2 direction;
+
+    public Transform player;
+
+    float speed = 3f;
+
+    bool triggered;
+   
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            triggered = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (triggered)
+        {
+            direction = player.position - transform.position;
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed);
+        }
+    }
 }
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -10,19 +10,45 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    float health;
+    float maxHealth = 100f;
+
     private void Start()
     {
+        health = 100f;
         rb = this.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw(("Vertical"));
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(health >= 0)
+        {
+            Die();
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * walkSpeed * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TakeDamage();
+    }
+
+    private void TakeDamage()
+    {
+        health -= 5;
+    }
+
+    void Die()
+    {
+        //figure out what happens here. 
+    }
+
+    
 }
